f9acb07857c4abd0d5e180e99431d6065b609209
Kirill <csgokirilll@mail.ru>
2023-02-15 19:14:05 +0500

    start main activity XML

--- app/src/main/java/com/example/homework3/MainActivity.java
+++ app/src/main/java/com/example/homework3/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.homework3;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Objects.requireNonNull(this.getSupportActionBar()).hide();
     }
 }
