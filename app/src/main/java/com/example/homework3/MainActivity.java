da1b97a4584d1dcd82edec191087ec53cc7086c4
Kirill <csgokirilll@mail.ru>
2023-02-16 19:08:43 +0500

    complete fragment

--- app/src/main/java/com/example/homework3/MainActivity.java
+++ app/src/main/java/com/example/homework3/MainActivity.java
@@ -6,10 +6,15 @@ import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
 import java.util.Objects;
 public class MainActivity extends AppCompatActivity {
 
-    private Button addNewTicketBtn;
+    private Button addNewTicketBtn, fragmentPlus, fragmentMinus;
+    public CheckBox adultTicketCheckBox, retireeTicketCheckBox, kidTicketCheckBox;
+    public TextView counterFragment, costFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Objects.requireNonNull(this.getSupportActionBar()).hide();
 
-        addNewTicketBtn = findViewById(R.id.new_ticket);
+        fillVariables();
 
         addNewTicketBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,5 +37,15 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.first_frame, fragment);
         ft.commit();
+    }
+    public void fillVariables(){
+        addNewTicketBtn = findViewById(R.id.new_ticket);
+        adultTicketCheckBox = findViewById(R.id.adult_check_box);
+        retireeTicketCheckBox = findViewById(R.id.retiree_check_box);
+        kidTicketCheckBox = findViewById(R.id.kid_check_box);
+        counterFragment = findViewById(R.id.counter_fragment);
+        costFragment = findViewById(R.id.cost_fragment);
+        fragmentMinus = findViewById(R.id.btn_count_minus);
+        fragmentPlus = findViewById(R.id.btn_count_plus);
     }
 }
--- app/src/main/java/com/example/homework3/TicketFragment.java
+++ app/src/main/java/com/example/homework3/TicketFragment.java
@@ -1,20 +1,13 @@
 package com.example.homework3;
 
-import android.annotation.SuppressLint;
-import android.graphics.Color;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.Objects;
-
 public class TicketFragment extends Fragment {
 
-    @SuppressLint("ResourceAsColor")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
