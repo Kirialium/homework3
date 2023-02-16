66fa5dd5fe054d49de16ccf8f67479d7ad0aefc3
Kirill <csgokirilll@mail.ru>
2023-02-16 21:45:10 +0500

    work on fragment logic

--- app/src/main/java/com/example/homework3/MainActivity.java
+++ app/src/main/java/com/example/homework3/MainActivity.java
@@ -6,15 +6,12 @@ import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.TextView;
-
 import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button addNewTicketBtn, fragmentPlus, fragmentMinus;
-    public CheckBox adultTicketCheckBox, retireeTicketCheckBox, kidTicketCheckBox;
-    public TextView counterFragment, costFragment;
+    private Button addNewTicketBtn;
+    private View lineBehind;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,23 +26,19 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 TicketFragment ticketFragment = new TicketFragment();
                 setNewFragment(ticketFragment);
+                lineBehind.setAlpha(1);
             }
         });
-
     }
-    public void setNewFragment(Fragment fragment){
+
+    private void setNewFragment(Fragment fragment){
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.first_frame, fragment);
         ft.commit();
     }
-    public void fillVariables(){
+
+    private void fillVariables(){
         addNewTicketBtn = findViewById(R.id.new_ticket);
-        adultTicketCheckBox = findViewById(R.id.adult_check_box);
-        retireeTicketCheckBox = findViewById(R.id.retiree_check_box);
-        kidTicketCheckBox = findViewById(R.id.kid_check_box);
-        counterFragment = findViewById(R.id.counter_fragment);
-        costFragment = findViewById(R.id.cost_fragment);
-        fragmentMinus = findViewById(R.id.btn_count_minus);
-        fragmentPlus = findViewById(R.id.btn_count_plus);
+        lineBehind = findViewById(R.id.line_behind_1);
     }
 }
--- app/src/main/java/com/example/homework3/TicketFragment.java
+++ app/src/main/java/com/example/homework3/TicketFragment.java
@@ -1,16 +1,94 @@
 package com.example.homework3;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
 public class TicketFragment extends Fragment {
 
+    final int turCostAdult = 70;
+    final int turCostKid = 35;
+    final int turCostRetiree = 21;
+    int costTicket = 0;
+    int count = 1;
+
+    @SuppressLint("SetTextI18n")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_ticket, container, false);
 
-        return inflater.inflate(R.layout.fragment_ticket, container, false);
+        Button btnCountMines = (Button) rootView.findViewById(R.id.btn_count_minus);
+        Button btnCountPlus = (Button) rootView.findViewById(R.id.btn_count_plus);
+        CheckBox adultCheckBox = (CheckBox) rootView.findViewById(R.id.adult_check_box);
+        CheckBox retireeCheckBox = (CheckBox) rootView.findViewById(R.id.retiree_check_box);
+        CheckBox kidCheckBox = (CheckBox) rootView.findViewById(R.id.kid_check_box);
+        TextView counterFragment = (TextView) rootView.findViewById(R.id.counter_fragment);
+        TextView costFragment = (TextView) rootView.findViewById(R.id.cost_fragment);
+
+        //Плюс и минус для каунта
+        btnCountMines.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(adultCheckBox )
+                if(count > 1){
+                    count--;
+                    counterFragment.setText((String.valueOf(count)));
+                    setCostFragment(costFragment);
+                }
+            }
+        });
+
+        btnCountPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(count != 10){
+                    count++;
+                    counterFragment.setText(String.valueOf(count));
+                    setCostFragment(costFragment);
+                }
+            }
+        });
+
+        //Чек боксы
+        adultCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                costTicket = turCostAdult * count;
+                setCostFragment(costFragment);
+                retireeCheckBox.setChecked(false);
+                kidCheckBox.setChecked(false);
+            }
+        });
+
+        retireeCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                costTicket = turCostRetiree * count;
+                setCostFragment(costFragment);
+                adultCheckBox.setChecked(false);
+                kidCheckBox.setChecked(false);
+            }
+        });
+        kidCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                costTicket = turCostKid * count;
+                setCostFragment(costFragment);
+                adultCheckBox.setChecked(false);
+                retireeCheckBox.setChecked(false);
+            }
+        });
+
+        return rootView;
+    }
+    private void setCostFragment(TextView costFragment){
+        costFragment.setText(String.valueOf(costFragment));
     }
 }
