86024c5e5c31681fd4710809143da135bdd82b67
Kirill <csgokirilll@mail.ru>
2023-02-16 17:19:26 +0500

    developing ticket design

--- app/src/main/java/com/example/homework3/MainActivity.java
+++ app/src/main/java/com/example/homework3/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.homework3;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
